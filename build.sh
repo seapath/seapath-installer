78c17e2ff02f4d33d9ecca4177532d98075e0ec4
Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>
2025-09-02 16:09:49 +0200

    build.sh: add settings.conf in seapath installer package
    This file was missing.
    
    Signed-off-by: Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>
    Signed-off-by: Aiswarya Kaitheri Kandoth <aiswarya.kandoth@savoirfairelinux.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ export BUILDDIR=./build
 export CMAKE_ARGS="-DWITH_QT6=OFF"
 
 ./ci/build.sh
-mkdir -p seapath-installer_1.0_all
+mkdir -p seapath-installer_1.0_all/etc/calamares
+cp settings.conf seapath-installer_1.0_all/etc/calamares
 cp -r ./debian/* seapath-installer_1.0_all/
 cp -r $DESTDIR/* seapath-installer_1.0_all/
 dpkg-deb --build seapath-installer_1.0_all
