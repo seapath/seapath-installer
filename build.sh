9f5724bcfd85df9f1f81e50e9b3d27b2078c8bc0
Luc BEAUFILS <luc.beaufils@savoirfairelinux.com>
2025-09-04 17:43:43 +0200

    build.sh: cp partition.conf into calamares modules directory
    Signed-off-by: Luc BEAUFILS <luc.beaufils@savoirfairelinux.com>
    Signed-off-by: Aiswarya Kaitheri Kandoth <aiswarya.kandoth@savoirfairelinux.com>

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ export CMAKE_ARGS="-DWITH_QT6=OFF"
 
 ./ci/build.sh
 mkdir -p seapath-installer_1.0_all/etc/calamares
+mkdir -p seapath-installer_1.0_all/usr/share/calamares/modules
 cp settings.conf seapath-installer_1.0_all/etc/calamares
+cp partition.conf seapath-installer_1.0_all/usr/share/calamares/modules
 cp -r ./debian/* seapath-installer_1.0_all/
 cp -r $DESTDIR/* seapath-installer_1.0_all/
 dpkg-deb --build seapath-installer_1.0_all
