e3b9acccb78a103dfe4db6c74ce002929aa8a6fc
Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>
2025-10-22 13:47:03 +0200

    modules/sshkeyselection: add support for multiple users
    Currently, SSH keys are only installed for the 'admin' user. Also
    install them for the 'ansible' user.
    
    Signed-off-by: Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>

--- src/modules/sshkeyselection/SshKeySelectionJob.cpp
+++ src/modules/sshkeyselection/SshKeySelectionJob.cpp
@@ -20,22 +20,19 @@ SshKeySelectionJob::exec()
 {
     cDebug() << "Executing SshKeySelectionJob";
     QDir destDir;
-    QString dotSSHConfPath;
+    QStringList dotSSHConfPath;
+    QStringList users = { "admin", "ansible" };
     Calamares::GlobalStorage* gs = Calamares::JobQueue::instance()->globalStorage();
     QString seapathFlavor = gs->value( "seapathFlavor" ).toString();
     cDebug() << "SshKeySelectionJob: Seapath flavor:" << seapathFlavor;
 
     if (seapathFlavor == "yocto"){
         destDir = QDir( gs->value( "homeMountPoint" ).toString() );
-        dotSSHConfPath = destDir.absoluteFilePath( "admin/.ssh" );
-        cDebug() << "SshKeySelectionJob: writing selected keys to" << dotSSHConfPath;
     }
 
     else if (seapathFlavor == "debian"){
         destDir = QDir( gs->value( "rootMountPoint" ).toString() );
         cDebug() << "SshKeySelectionJob: Debian root mount point:" << destDir.absolutePath();
-        dotSSHConfPath = destDir.absoluteFilePath( "home/admin/.ssh" );
-        cDebug() << "SshKeySelectionJob: writing selected keys to" << dotSSHConfPath;
     }
     else {
         cError() << "SshKeySelectionJob: Unknown Seapath flavor:" << seapathFlavor;
@@ -44,6 +41,10 @@ SshKeySelectionJob::exec()
             tr("Unknown Seapath flavor: %1.", "@error, %1 is the Seapath flavor").arg(seapathFlavor));
     }
 
+    for (const QString& user : users) {
+        dotSSHConfPath.append(destDir.absoluteFilePath(user + QStringLiteral("/.ssh")));
+        cDebug() << "SshKeySelectionJob: writing selected keys to" << dotSSHConfPath;
+    }
     cDebug() << "SshKeySelectionJob: Destination directory:" << destDir.absolutePath();
 
     QStringList sshKeys = gs->value( "sshkeyselection.selectedKeys" ).toStringList();
@@ -55,39 +56,41 @@ SshKeySelectionJob::exec()
 
         for (const QString& keyPath : sshKeys)
         {
-            cDebug() << "SshKeySelectionJob: processing key" << keyPath;
-            QFile srcFile(keyPath);
-            if (!srcFile.open(QIODevice::ReadOnly))
-            {
-                cError() << "SshKeySelectionJob: cannot open source key file" << srcFile.fileName();
-                return Calamares::JobResult::error(
-                    tr("Failed to open SSH key file.", "@error"),
-                    tr("Could not open SSH key file %1 for reading.", "@error, %1 is the SSH key file path").arg(srcFile.fileName()));
+            for (const QString& dotSSHPath : dotSSHConfPath) {
+                QString destFilePath = dotSSHPath + "/authorized_keys";
+                cDebug() << "SshKeySelectionJob: processing key" << keyPath;
+                QFile srcFile(keyPath);
+                if (!srcFile.open(QIODevice::ReadOnly))
+                {
+                    cError() << "SshKeySelectionJob: cannot open source key file" << srcFile.fileName();
+                    return Calamares::JobResult::error(
+                        tr("Failed to open SSH key file.", "@error"),
+                        tr("Could not open SSH key file %1 for reading.", "@error, %1 is the SSH key file path").arg(srcFile.fileName()));
+                }
+
+                QFile destFile(destFilePath);
+                if (!destFile.open(QIODevice::Append))
+                {
+                    cError() << "SshKeySelectionJob: cannot open destination key file" << destFile.fileName();
+                    return Calamares::JobResult::error(
+                        tr("Failed to write SSH key file.", "@error"),
+                        tr("Could not open SSH key file %1 for writing.", "@error, %1 is the SSH key file path").arg(destFile.fileName()));
+                }
+
+                destFile.write(srcFile.readAll());
+                srcFile.close();
+                destFile.close();
+
+                if (destFile.error() != QFile::NoError)
+                {
+                    cError() << "SshKeySelectionJob: error writing to destination key file" << destFile.fileName();
+                    return Calamares::JobResult::error(
+                        tr("Failed to write SSH key file.", "@error"),
+                        tr("Error occurred while writing SSH key file %1.", "@error, %1 is the SSH key file path").arg(destFile.fileName()));
+                }
+
+                cDebug() << "SshKeySelectionJob: successfully copied key to" << destFilePath;
             }
-
-            QString destFilePath = dotSSHConfPath + "/authorized_keys";
-            QFile destFile(destFilePath);
-            if (!destFile.open(QIODevice::Append))
-            {
-                cError() << "SshKeySelectionJob: cannot open destination key file" << destFile.fileName();
-                return Calamares::JobResult::error(
-                    tr("Failed to write SSH key file.", "@error"),
-                    tr("Could not open SSH key file %1 for writing.", "@error, %1 is the SSH key file path").arg(destFile.fileName()));
-            }
-
-            destFile.write(srcFile.readAll());
-            srcFile.close();
-            destFile.close();
-
-            if (destFile.error() != QFile::NoError)
-            {
-                cError() << "SshKeySelectionJob: error writing to destination key file" << destFile.fileName();
-                return Calamares::JobResult::error(
-                    tr("Failed to write SSH key file.", "@error"),
-                    tr("Error occurred while writing SSH key file %1.", "@error, %1 is the SSH key file path").arg(destFile.fileName()));
-            }
-
-            cDebug() << "SshKeySelectionJob: successfully copied key to" << destFilePath;
         }
 
 
