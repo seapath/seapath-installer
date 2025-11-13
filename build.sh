51744f61c456422cd8f48209a1bcb58bd2f53639
Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>
2025-11-13 14:29:59 +0100

    build.sh: bump .deb package version to 1.1.0
    Signed-off-by: Paul Le Guen de Kerneizon <paul.leguendekerneizon@savoirfairelinux.com>

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 export DESTDIR=/tmp/sysroot-seapath-installer
 export BUILDDIR=./build
 export CMAKE_ARGS="-DWITH_QT6=OFF"
-
+export VERSION="1.1.0"
 ./ci/build.sh
-mkdir -p seapath-installer_1.0_all/etc/calamares
-mkdir -p seapath-installer_1.0_all/usr/share/calamares/modules
-cp settings.conf seapath-installer_1.0_all/etc/calamares
-cp partition.conf seapath-installer_1.0_all/usr/share/calamares/modules
-cp -r ./debian/* seapath-installer_1.0_all/
-cp -r $DESTDIR/* seapath-installer_1.0_all/
-dpkg-deb --build seapath-installer_1.0_all
+mkdir -p seapath-installer_${VERSION}_all/etc/calamares
+mkdir -p seapath-installer_${VERSION}_all/usr/share/calamares/modules
+cp settings.conf seapath-installer_${VERSION}_all/etc/calamares
+cp partition.conf seapath-installer_${VERSION}_all/usr/share/calamares/modules
+cp -r ./debian/* seapath-installer_${VERSION}_all/
+cp -r $DESTDIR/* seapath-installer_${VERSION}_all/
+dpkg-deb --build seapath-installer_${VERSION}_all
